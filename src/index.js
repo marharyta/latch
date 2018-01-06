f377be636f1c7678ce2e3cf5a93d4ec4f1ab37f5
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-01-06 21:41:11 +0200

    Quit attempts to configure css loaders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styles from "./index.css";
 
 const Test = () => <div className={styles.wrapper}> Hello </div>;
 
--- webpack.config.js
+++ webpack.config.js
@@ -20,25 +20,19 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract(
-          // combineLoaders([
-          //   {
-          //     loader: "style-loader"
-          //   },
-          //   {
-          //     loader: "css-loader",
-          //     query: {
-          //       modules: true,
-          //       localIdentName: "[name]__[local]___[hash:base64:5]"
-          //     }
-          //   }
-          //   // {
-          //   //   loader: "sass-loader"
-          //   // }
-          // ])
-          {
-            use: "css-loader"
-          }
+        loader: ExtractTextPlugin.extract(
+          combineLoaders([
+            {
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader",
+              query: {
+                modules: true,
+                localIdentName: "[name]__[local]___[hash:base64:5]"
+              }
+            }
+          ])
         )
       }
     ]
